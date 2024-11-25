eea30056791dd090eaecf70acf01834fb6a46374
Hanqing Zhang <136445200+hz1021@users.noreply.github.com>
2024-11-25 22:09:06 +0000

    Delete Lin_InterPoints.m

--- Lin_InterPoints.m
+++
@@ -1,9 +0,0 @@
-%% SignalGenerator 
-% Interpolation points
-s = linspace(0, 6.1, 10);
-upsilon = s + 0.1;
-eigenS = 10.^s.*1i;
-eigenQ = 10.^upsilon.*1i;
-
-eigenS_multiplicity = ones(1, size(eigenS, 2));
-eigenQ_multiplicity = ones(1, size(eigenQ, 2));
